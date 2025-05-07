3a7bc97b086fb0fcbb9d90dfdf9f6fa0bc056556
Aaidt <aadits2004@gmail.com>
2025-05-07 22:03:03 +0530

    fe routes

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,11 +1,21 @@
-import { useState } from "react";
+import { Routes, Route, BrowserRouter } from "react-router-dom"
+import { Signin } from "./components/ui/Signin"
+import { Signup } from "./components/ui/Signup"
+import { LandingPage } from "./pages/LandingPage"
+import { Dashboard } from "./pages/Dashboard"
+import { Transaction } from "./pages/Transaction"
+
 function App() {
-  const [count, setCount] = useState<number>(0)
   return (
-    <div className="h-full min-h-screen bg-black/90 text-white/90 flex justify-center items-center ">
-      Count: {count}
-      <button onClick={() => setCount(count + 1)}>Click me!</button>
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<LandingPage />} />
+        <Route path="/signup" element={<Signup />} />
+        <Route path="/sigin" element={<Signin />} />
+        <Route path="/dashboard" element={<Dashboard />} />
+        <Route path="/send" element={<Transaction />} />
+      </Routes>
+    </BrowserRouter>
   )
 }
 
