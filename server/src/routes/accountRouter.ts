d2b499e5c89d01a93ad5d1c0face47cab944206b
Aaidt <aadits2004@gmail.com>
2025-05-07 20:44:45 +0530

    transaction route logic

--- server/src/routes/accountRouter.ts
+++ server/src/routes/accountRouter.ts
@@ -19,5 +19,54 @@ router.get("/balance", authMiddleware, async (req: Request, res: Response) => {
 
 })
 
+router.post("/transfer", authMiddleware, async (req: Request, res: Response) => {
+    const { to, amount } = req.body;
+
+    try {
+        const account = await AccountModel.findOne({
+            userId: req.userId
+        });
+
+        if (!account?.balance) {
+            console.log("No balance.")
+            return
+        }
+
+        if (account?.balance < amount) {
+            res.status(400).json({ message: "Insufficient balance" })
+            return
+        }
+
+        const toAccount = await AccountModel.findOne({
+            userId: to
+        });
+        if (!toAccount) {
+            res.status(400).json({ message: "Invalid account" });
+        }
+
+        await AccountModel.findOne({
+            userId: req.userId
+        }, {
+            $inc: {
+                balance: -amount
+            }
+        })
+
+        await AccountModel.findOne({
+            userId: to
+        }, {
+            $inc: {
+                balance: amount
+            }
+        })
+
+        res.status(200).json({ message: "Transaction successfull" });
+
+    } catch (err) {
+        res.status(500).json({ message: "Internal server error " + err })
+        console.log(err)
+    }
+})
+
 
 export default router
