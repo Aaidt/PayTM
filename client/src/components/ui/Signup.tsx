6e5d03ad728cf90c93d8bc040130e2e32469874d
Aaidt <aadits2004@gmail.com>
2025-05-08 15:33:22 +0530

    Signup fe done.

--- client/src/components/ui/Signup.tsx
+++ client/src/components/ui/Signup.tsx
@@ -1,6 +1,43 @@
 export const Signup = () => {
     return (
-        <div>
+        <div className="bg-gray-700/70 min-h-screen">
+            <div className="flex justify-center items-center container">
+                <div className="p-6 bg-white h-120 w-82 rounded-md mt-15 ml-20 shadow-lg shadow-black/60">
+                    <div className="flex flex-col justify-center items-center">
+                        <h1 className="font-bold text-3xl">Sign Up</h1>
+                        <p className="text-gray-500 pt-1">Enter your information to create an account</p>
+                    </div>
+                    <div className="">
+
+                        <div className="pt-2">
+                            <h3 className="font-semibold">First name:</h3>
+                            <input className="border-1 rounded-md w-full p-1 border-gray-400 hover:border-gray-800" placeholder="John"></input>
+                        </div>
+                        <div className="pt-2">
+                            <h3 className="font-semibold">Last name:</h3>
+                            <input className="border-1 rounded-md w-full p-1 border-gray-400 hover:border-gray-800" placeholder="Doe"></input>
+                        </div>
+                        <div className="pt-2">
+                            <h3 className="font-semibold">Email:</h3>
+                            <input className="border-1 rounded-md w-full p-1 border-gray-400 hover:border-gray-800" placeholder="JohnDoe@example.com"></input>
+                        </div>
+                        <div className="pt-2">
+                            <h3 className="font-semibold">Password:</h3>
+                            <input className="border-1 rounded-md w-full p-1 border-gray-400 hover:border-gray-800" type="password"></input>
+                        </div>
+
+                    </div>
+
+                    <div className="pt-4">
+                        <button className="hover:-translate-y-1 duration-200 cursor-pointer bg-black rounded-md w-full text-white p-1 flex justify-center items-center">Sign Up</button>
+                    </div>
+
+                    <div className="pt-2 flex items-center justify-center">
+                        Already have account?<div className="cursor-pointer hover:-translate-y-1 duration-200 underline underline-offset-3">Log In</div>
+                    </div>
+
+                </div>
+            </div>
 
         </div>
     )
