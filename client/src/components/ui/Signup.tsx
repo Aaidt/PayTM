68ebcf47db5824a32f191422bd57039c7365d6b2
Aaidt <aadits2004@gmail.com>
2025-05-08 16:03:43 +0530

    signup request logic and routing to dashboard.

--- client/src/components/ui/Signup.tsx
+++ client/src/components/ui/Signup.tsx
@@ -1,4 +1,37 @@
+import { useRef } from "react";
+import { useNavigate } from "react-router-dom";
+import axios from "axios";
+
+const BACKEND_URL = import.meta.env.VITE_BACKEND_URL
+
 export const Signup = () => {
+    const navigate = useNavigate();
+
+    const firstnameRef = useRef<HTMLInputElement>(null);
+    const lastnameRef = useRef<HTMLInputElement>(null);
+    const emailRef = useRef<HTMLInputElement>(null);
+    const passwordRef = useRef<HTMLInputElement>(null);
+
+    const handleSignup = async () => {
+        try {
+            const response = await axios.post(`${BACKEND_URL}/api/v1/user/signup`, {
+                firstname: firstnameRef.current?.value,
+                lastname: lastnameRef.current?.value,
+                username: emailRef.current?.value,
+                password: passwordRef.current?.value
+            });
+
+            alert("You have successfully Signed-Up!!!✅✅");
+            navigate("/dashboard");
+
+            console.log(response.data);
+
+        } catch (err) {
+            console.log("Something went wrong..." + err);
+        }
+
+    }
+
     return (
         <div className="bg-gray-700/70 min-h-screen">
             <div className="flex justify-center items-center container">
@@ -11,29 +44,63 @@ export const Signup = () => {
 
                         <div className="pt-2">
                             <h3 className="font-semibold">First name:</h3>
-                            <input className="border-1 rounded-md w-full p-1 border-gray-400 hover:border-gray-800" placeholder="John"></input>
+                            <input ref={firstnameRef}
+                                onKeyDown={(e) => {
+                                    if (e.key === "Enter") {
+                                        lastnameRef.current?.focus()
+                                    }
+                                }}
+                                className="border-1 rounded-md w-full p-1 border-gray-400 hover:border-gray-800"
+                                placeholder="John"></input>
                         </div>
+
                         <div className="pt-2">
                             <h3 className="font-semibold">Last name:</h3>
-                            <input className="border-1 rounded-md w-full p-1 border-gray-400 hover:border-gray-800" placeholder="Doe"></input>
+                            <input ref={lastnameRef}
+                                onKeyDown={(e) => {
+                                    if (e.key === "Enter") {
+                                        emailRef.current?.focus()
+                                    }
+                                }}
+                                className="border-1 rounded-md w-full p-1 border-gray-400 hover:border-gray-800"
+                                placeholder="Doe"></input>
                         </div>
+
                         <div className="pt-2">
                             <h3 className="font-semibold">Email:</h3>
-                            <input className="border-1 rounded-md w-full p-1 border-gray-400 hover:border-gray-800" placeholder="JohnDoe@example.com"></input>
+                            <input ref={emailRef}
+                                onKeyDown={(e) => {
+                                    if (e.key === "Enter") {
+                                        passwordRef.current?.focus()
+                                    }
+                                }}
+                                className="border-1 rounded-md w-full p-1 border-gray-400 hover:border-gray-800"
+                                placeholder="JohnDoe@example.com"></input>
                         </div>
+
                         <div className="pt-2">
                             <h3 className="font-semibold">Password:</h3>
-                            <input className="border-1 rounded-md w-full p-1 border-gray-400 hover:border-gray-800" type="password"></input>
+                            <input ref={passwordRef}
+                                className="border-1 rounded-md w-full p-1 border-gray-400 hover:border-gray-800"
+                                type="password"></input>
                         </div>
 
                     </div>
 
                     <div className="pt-4">
-                        <button className="hover:-translate-y-1 duration-200 cursor-pointer bg-black rounded-md w-full text-white p-1 flex justify-center items-center">Sign Up</button>
+                        <button onClick={() => handleSignup()}
+                            className="hover:-translate-y-1 duration-200 cursor-pointer bg-black rounded-md w-full text-white p-1 flex justify-center items-center">
+                            Sign Up
+                        </button>
                     </div>
 
-                    <div className="pt-2 flex items-center justify-center">
-                        Already have account?<div className="cursor-pointer hover:-translate-y-1 duration-200 underline underline-offset-3">Log In</div>
+                    <div className="pt-2 flex items-center justify-center text-md">
+                        Already have account?
+                        <div
+                            onClick={() => navigate("/signin")}
+                            className="cursor-pointer hover:-translate-y-1 duration-200 underline underline-offset-3">
+                            Log In
+                        </div>
                     </div>
 
                 </div>
--- client/src/pages/Dashboard.tsx
+++ client/src/pages/Dashboard.tsx
@@ -1,5 +1,5 @@
 export const Dashboard = () => {
-    return <div>
-        
+    return <div className="bg-black min-h-screen text-white flex justify-center items-center">
+        <p className="text-4xl ">Dashboard</p>
     </div>
 }
